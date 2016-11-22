e4ac09b1b71f934fba16ca33057af4f8b67c9b48
Charles Oliver <cpoliver@gmail.com>
2016-11-22 22:56:15 +0000

    added solution for "sum of multiples"

--- sum-of-multiples/sum_of_multiples.exs
+++ sum-of-multiples/sum_of_multiples.exs
@@ -4,6 +4,21 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    1..(limit - 1)
+    |> Enum.flat_map(fn number ->
+      factors
+      |> Enum.map(fn factor ->
+        case is_factor?(number, factor) do
+          true -> number
+          false -> 0
+        end
+      end)
+    end)
+    |> Enum.uniq
+    |> Enum.sum
+  end
 
+  def is_factor?(number, factor) do
+    number >= factor && rem(number, factor) == 0
   end
 end
